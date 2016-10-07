4351673a0c4def52798d3c9c46d3837a290c8caf
null null null <null@affinitygamescollective.org>
2016-10-07 08:47:53 -0700

    update deps

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 /* jshint node: true */
 'use strict';
 
-function getParentApp(app) {
-  if (typeof app.import !== 'function' && app.app) {
-    return getParentApp(app.app);
-  } else {
-    return app;
-  }
-}
-
 module.exports = {
-  name: 'affinity-engine-stage-direction-pause',
-
-  included: function(app) {
-    this._super.included(app);
-
-    this.eachAddonInvoke('safeIncluded', [app]);
-
-    app = getParentApp(app);
-  },
-
-  safeIncluded: function(app, parent) {
-    this.included(app, parent);
-  }
+  name: 'affinity-engine-stage-direction-pause'
 };
